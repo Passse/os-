f7fe52f2bfa30c8e83d547d1626f49e4081ecf7b
鲷鱼 <2382391761@qq.com>
2019-09-11 11:02:18 +0800

    Create main.swift

---
+++ 9.11/9.11/main.swift
@@ -0,0 +1,27 @@
+/*var i=1
+while i<=100
+{
+    print(i)
+    i = i + 1
+}
+
+repeat{
+    print(i)
+    i = i + 1
+}while i<=100 */
+/*var i = 1
+var s = 0
+while i <= 100
+{
+    s = s + i
+    i = i + 1
+}
+print(s)*/
+var i = readLine()!
+while i != "p"
+{
+    var j = Int(i)!
+    var result = j * 2
+    print(result)
+    i = readLine()!
+}
